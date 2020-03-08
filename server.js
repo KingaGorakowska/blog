9aa9504b1b7776bdc2a010b62db7244fa6b42d52
KingaGorakowska <k.gorakowska@gmail.com>
2020-03-08 11:22:05 +0100

    add new content

--- server.js
+++ server.js
@@ -9,16 +9,63 @@ let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//table with post list - object (literal)
+let posts = [];
+
 //starting the template engine for module ejs
 app.set("view engine", "ejs");
+
+//defining of server paths
+app.use(express.static(__dirname + '/'));
 
 //start server in 8080 localhost
 app.listen(8080);
 console.log("Server success start in 8080 localhost");
 
 app.get("/", function(req, res){
+	res.render("index", {posts:posts});
+});
+
+app.get("/insertPost", function(req, res){
+	res.render("insertPost");	
+});
+
+app.post("/insertPost", function(req, res){
+	let obj = {
+		title: req.body['title'],
+		description: req.body['description']
+	}
 	
-	res.render("index", {name:"wordName :)"});
+	posts.push(obj);
+	res.render("index", {posts:posts});
+});
+
+app.get("/details/:id", function(req, res){
+	let id = req.params.id;
+	res.render("details", {post:posts[id]});
+});
+
+app.get("/delete/:id", function(req, res){
+	let id = req.params.id;
+	posts.splice(id, 1);
 	
+	res.render("index", {posts:posts});
+});
+
+app.get("/editPost/:id", function(req, res){
 	
+	let id = req.params.id;
+	res.render("editPost", {post:posts[id], id:id});	
+});
+
+app.post("/editPost/:id", function(req, res){
+	let id = req.params.id;
+	
+	let obj = {
+		title: req.body['title'],
+		description: req.body['description']
+	}
+	
+	posts[id] = obj;
+	res.render("index", {posts:posts});
 });
